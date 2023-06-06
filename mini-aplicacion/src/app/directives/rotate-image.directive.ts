442e72418f986986ec63dae7e11dcdedda8c1504
DESKTOP-K2GASL6\Angel <carmenFGDev@gmail.com>
2023-06-06 20:30:17 +0200

    feature: fix directive rotate

--- mini-aplicacion/src/app/directives/rotate-image.directive.ts
+++ mini-aplicacion/src/app/directives/rotate-image.directive.ts
@@ -4,15 +4,21 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[appRotateImage]',
 })
 export class RotateImageDirective {
-  @Input() rotate: number = 180;
   @Input() step: number = 10;
 
   constructor(private el: ElementRef) {}
 
   @HostListener('mousedown')
   onMouseEnter() {
-    const arg = 'rotate(' + this.rotate.toString() + 'deg)';
-    this.el.nativeElement.style.transform = arg;
+   const currentRotation = this.el.nativeElement.style.transform;
+    const currentDegrees = parseInt(
+      currentRotation
+        ? currentRotation.replace('rotate(', '').replace('deg)', '')
+        : '0',
+      10
+    );
+    const newRotation = `rotate(${currentDegrees + this.step}deg)`;
+    this.el.nativeElement.style.transform = newRotation;
     this.el.nativeElement.transition = 'all 4s ease';
   }
 }
